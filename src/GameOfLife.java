bc1142724827fe7696c3c199351f4be9323dee17
Hakim Bin Luqman <binluqma@polytech.unice.fr>
2014-04-19 17:44:00 +0200

    First commit

---
+++ src/GameOfLife.java
@@ -0,0 +1,77 @@
+import exceptions.InitializingPatternOutOfBoundsException;
+import gui.GridView;
+import gui.MainView;
+import model.Grid;
+import model.Pattern;
+
+import java.io.IOException;
+
+/**
+ * Created by Hakim on 19/04/14.
+ */
+public class GameOfLife{
+
+    public static void main(String argv[])
+            throws InitializingPatternOutOfBoundsException {
+        System.setProperty("java.util.Arrays.useLegacyMergeSort", "true");
+        GameOfLife life = new GameOfLife(20, 20);
+        byte bpattern[][] = {{1, 1, 0, 0},
+                             {1, 1, 0, 0},
+                             {0, 0, 1, 1},
+                             {0, 0, 1, 1}};
+        life.grid.initializeWithPattern(new Pattern(bpattern));
+        life.run();
+    }
+
+    private Grid grid;
+    private MainView mainView ;
+    private GridView gridView;
+
+    public GameOfLife() { this(25, 25); }
+
+    public GameOfLife(int width, int height) {
+        grid = new Grid(width, height);
+        mainView = new MainView(width, height);
+        gridView = mainView.getGridView();
+
+        for(int x = 0; x < width; x ++) {
+            for (int y = 0; y < height; y++) {
+                grid.getCell(x, y).setListener(gridView);
+            }
+        }
+    }
+
+    public void run() {
+        int generation = 0;
+        while(true) {
+            System.out.println("generation: " + generation);
+
+            for(int x = 0; x < grid.getWidth(); x ++) {
+                for (int y = 0; y < grid.getHeight(); y++) {
+                    //System.out.println(grid.getCell(x, y));
+                    grid.getCell(x, y).calculateNextStep();
+                }
+            }
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                System.in.read();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            for(int x = 0; x < grid.getWidth(); x ++) {
+                for (int y = 0; y < grid.getHeight(); y++) {
+                    grid.getCell(x, y).applyNextStep();
+                }
+            }
+
+            generation++;
+        }
+    }
+}
---
+++ src/model/Cell.java
@@ -0,0 +1,62 @@
+package model;
+
+import gui.CellChangedEvent;
+import gui.CellChangedListener;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Hakim on 19/04/14.
+ */
+public class Cell {
+    private int xCoordinate;
+    private int yCoordinate;
+    private boolean alive;
+    private boolean willLive = false;
+    private ArrayList<Cell> neighbours;
+    private CellChangedListener listener;
+
+    private int livingNeighboursCount() {
+        int livingNeighbours = 0;
+        for(Cell c : neighbours) { if (c.isAlive()) livingNeighbours++;}
+        return livingNeighbours;
+    }
+
+    public  void calculateNextStep() {
+        /*Determine the next step (whether the cell will live or not) based on the
+          number of living neighbours and applying the rules of the Game of Life. */
+        switch(livingNeighboursCount()) {
+            case 2: willLive = isAlive(); break;
+            case 3: willLive = true; break;
+            default: willLive = false;
+        }
+    }
+
+    public void applyNextStep() {
+        if(alive != willLive) {
+            alive = willLive;
+            listener.cellChanged(new CellChangedEvent(this));
+
+            System.out.print("Cell [" + xCoordinate + ", " + yCoordinate +"] is now ");
+            if(alive) System.out.println("alive"); else System.out.println("dead");
+        }
+    }
+
+    public boolean isAlive() { return this.alive; }
+    public void setAlive(boolean alive) { this.alive = alive; listener.cellChanged(new CellChangedEvent(this));}
+
+    public void setCoordinates(int x, int y)     {xCoordinate = x; yCoordinate = y;}
+    public int getxCoordinate() { return xCoordinate; }
+    public int getyCoordinate() { return yCoordinate; }
+
+    public ArrayList<Cell> getNeighbours() { return neighbours; }
+    public void setNeighbours(ArrayList<Cell> neighbours) {this.neighbours = neighbours;}
+    public void setListener(CellChangedListener listener) { this.listener = listener; }
+
+    @Override
+    public String toString(){
+        String id = Integer.toString(xCoordinate) + ", " + Integer.toString(yCoordinate) + " nghbrs = " + livingNeighboursCount();
+        if(alive) return  id + " - alive";
+        else      return  id + " - dead";
+    }
+}
